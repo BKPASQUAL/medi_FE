eac0ed200a9bebd47c81f8bc914f649c3c8522ec
Bawantha <bawanthapasqual@gmail.com>
2023-12-31 23:03:12 +0530

    sa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Customize the port number
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components', // Create custom aliases for paths
+    },
+  },
 })
